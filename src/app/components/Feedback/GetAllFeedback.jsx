1e1da78557d8f9a5d8d753231d485d26bd2448fc
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2025-02-09 00:59:09 +0200

    Updated2

--- src/app/components/Feedback/GetAllFeedback.jsx
+++ src/app/components/Feedback/GetAllFeedback.jsx
@@ -56,9 +56,7 @@ export default function FeedbackList({ feedbacks }) {
                 <motion.img
                   src={feedback.imageUrl}
                   alt={feedback.name}
-                  width={40}
-                  height={40}
-                  className="w-20 h-20 rounded-full object-contain border-2 border-primary p-0 shadow-md shadow-primary"
+                  className="w-16 h-16 rounded-full object-contain border-2 border-primary p-0 shadow-md shadow-primary"
                 />
                 <div>
                   <h2 className="card-title text-base-100 text-lg font-bold">{feedback.name}</h2>
