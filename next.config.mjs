fad96156521e4e40bf9ed3549ef8f8e595c51ef9
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2025-02-12 21:47:27 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // allow for vercel links
+    experimental: {
+        linksInProduction: true,
+    },
     images: {
         domains: ['a8mzjiayucvjwt55.public.blob.vercel-storage.com'],
     },
--- src/app/visits/page.js
+++ src/app/visits/page.js
@@ -1,44 +1,21 @@
-"use client";
-import { useEffect, useState } from "react";
+export default function Visits() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-base-100">
+            {/* Page Title */}
+            <h2 className="text-3xl font-bold text-neutral mb-8">
+                Analytic Page
+            </h2>
 
-export default function VisitCount() {
-  const [visitors, setVisitors] = useState([]);
-
-  useEffect(() => {
-    async function fetchVisitors() {
-      const res = await fetch("/api/visit");
-      const data = await res.json();
-      setVisitors(data);
-    }
-
-    fetchVisitors();
-  }, []);
-
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Recent Visitors</h1>
-      <table className="w-full mt-4 border-collapse border border-gray-300">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border border-gray-300 p-2">Email</th>
-            <th className="border border-gray-300 p-2">IP</th>
-            <th className="border border-gray-300 p-2">Country</th>
-            <th className="border border-gray-300 p-2">City</th>
-            <th className="border border-gray-300 p-2">Visited At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {visitors.map((visitor, index) => (
-            <tr key={index} className="text-center border border-gray-300">
-              <td className="border border-gray-300 p-2">{visitor.email}</td>
-              <td className="border border-gray-300 p-2">{visitor.ip}</td>
-              <td className="border border-gray-300 p-2">{visitor.country}</td>
-              <td className="border border-gray-300 p-2">{visitor.city}</td>
-              <td className="border border-gray-300 p-2">{visitor.visitedAt}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+            {/* Link Button */}
+            <a
+                href="https://vercel.com/abdelazizsleem01s-projects/as-portfolio/analytics"
+                target="_blank"
+                rel="noopener noreferrer" 
+                className=" px-6 py-3 bg-primary text-white font-semibold rounded-lg shadow-md hover:bg-primary/95 transition duration-300 ease-in-out transform hover:scale-105"
+                title="Visits Analytics"
+            >
+                View Analytics on Vercel
+            </a>
+        </div>
+    );
 }
