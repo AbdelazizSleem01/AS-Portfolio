0fae2691f21a3e8461f113f40594e6bb87f31026
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2025-02-22 18:55:09 +0200

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -77,7 +77,9 @@ export const metadata = {
     card: "summary_large_image",
     title: "Abdelaziz Sleem | Fullstack Developer",
     description: "Passionate Fullstack Developer specializing in Frontend Development. Expertise in React, Next.js, Node.js, and MongoDB.",
-    creator: "@Abdelaziz Sleem"
+    creator: "@Abdelaziz Sleem",
+    images: ["/favicon.ico"],
+
   },
   robots: {
     index: true,
@@ -100,13 +102,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning >
+      <head>
+        <link rel="canonical" href="https://as-portfolio-ten.vercel.app/" />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
             <Suspense fallback={
-              // loading spinner
               <div className="flex justify-center items-center h-screen w-screen bg-gray-100 text-gray-600">
-                <svg className="animate-spin h-10 w-10 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <svg
+                  className="animate-spin h-10 w-10 text-indigo-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  aria-label="Loading"
+                >
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
                 <p className="mt-2 text-gray-500">Loading...</p>
               </div>
